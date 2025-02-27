3064d796f3a9535f6179fdfa73ce9a5f0547c017
sukendhar <sukendhar555@gmail.com>
2025-02-27 16:39:37 -0600

    Prevent payments that exceed the remaining amount of a loan

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,4 +3,15 @@ class Payment < ActiveRecord::Base
 
   validates :amount, presence: true
   validates :date, presence: true
+
+  validate :amount_does_not_exceed
+
+  private
+  def amount_does_not_exceed
+    return unless loan && amount
+
+    if amount > loan.remaining_amount
+      errors.add(:amount, "cannot exceed the remaining amount of the loan")
+    end
+  end
 end
