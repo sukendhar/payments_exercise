576af751b89dae7641597ce39cab9ab580c11d3a
sukendhar <sukendhar555@gmail.com>
2025-02-27 16:57:43 -0600

    Fix excluding the current payment

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,6 +2,6 @@ class Loan < ActiveRecord::Base
   has_many :payments, dependent: :destroy
 
   def remaining_amount
-    funded_amount - payments.sum(&:amount)
+    funded_amount - payments.sum(:amount)
   end
 end
